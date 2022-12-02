a85960eeb0361d5cc5d79d442756a24892851d63
BigTamz <bigtamz@bigtamzs-mbp.home>
2022-12-02 10:50:08 +0000

    basic terraform operations

--- main.tf
+++ main.tf
@@ -31,5 +31,5 @@ resource "azurerm_subnet" "examplesubnet" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.examplerg.name
   virtual_network_name = azurerm_virtual_network.examplevnet.name
-  address_space       = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 }
